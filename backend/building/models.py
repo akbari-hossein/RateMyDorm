94f1be1437d4eae8a0415421abb989184d0e6721
Soroushbiro84 <166137295+Soroushbiro84@users.noreply.github.com>
2026-06-10 10:22:19 +0330

    feat(backend): successfully generate and apply database migrations for all models

--- backend/building/models.py
+++ backend/building/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from university.models import University
 
 class Building(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'male'),
+        ('F', 'female'),
+    ]
     university = models.ForeignKey(University, on_delete=models.CASCADE, related_name='building')
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255, blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
+    facilities = models.TextField(blank=True, null=True)
 
+    class Meta:
+        unique_together = ('university', 'name')
+        
     def __str__(self):
         return f"{self.name} ({self.university.name})"
